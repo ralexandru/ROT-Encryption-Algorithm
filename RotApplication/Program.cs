c00421491e81e75009a492d0e620451e17f2ae00
araducu <raducu.alexandru123@gmail.com>
2022-02-08 15:49:04 +0200

    Using the RotAlgorithm class you can encrypt/decrypt a text using any kind of ROT Algorithm(1,2,3..n). You can also check the type of ROT Algorithm used to encrypt a text.

---
+++ RotApplication/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RotApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RotAlgorithm rot13 = new RotAlgorithm(); //Create an object of RotAlgorithm class specifing the number of rotations if nothing is specified, then ROT13 algorithm is used by default)
+            string textEncryptedWithRot13 = RotAlgorithm.Encrypt(rot13, "An example of encrypted text. (ROT13)"); //To encrypt a text, call the static method Encrypt of the RotAlgorithm class that returns the encrypted string.
+            Console.WriteLine("ENCRYPTED USING ROT13: " + textEncryptedWithRot13);
+            //Output: ENCRYPTED USING ROT13: Na rknzcyr bs rapelcgrq grkg. (EBG13)
+
+            RotAlgorithm rot3 = new RotAlgorithm(3);
+            string textEncryptedWithRot3 = RotAlgorithm.Encrypt(rot3, "An example of encrypted text. (ROT3)");
+            Console.WriteLine("ENCRYPTED USING ROT3: " + textEncryptedWithRot3);
+            //Output: ENCRYPTED USING ROT3: Dq hadpsoh ri hqfubswhg whaw. (URW3)
+
+            RotAlgorithm rot5 = new RotAlgorithm(5);
+            string textEncryptedWithRot5 = RotAlgorithm.Encrypt(rot5, "An example of encrypted text. (ROT5)");
+            Console.WriteLine("ENCRYPTED USING ROT5: " + textEncryptedWithRot5);
+            //Output: ENCRYPTED USING ROT5: Fs jcfruqj tk jshwduyji yjcy. (WTY5)
+
+            // Decrypting the encrypted text
+            Console.WriteLine(RotAlgorithm.Decrypt(rot13, textEncryptedWithRot13));
+            //Output: An example of encrypted text. (ROT13)
+
+            Console.WriteLine(RotAlgorithm.Decrypt(rot3, textEncryptedWithRot3));
+            //Output: An example of encrypted text. (ROT3)
+
+            Console.WriteLine(RotAlgorithm.Decrypt(rot5, textEncryptedWithRot5));
+            //Output: An example of encrypted text. (ROT5)
+
+            string decryptedText = "Text to encrypt";
+            string encryptedText = RotAlgorithm.Encrypt(new RotAlgorithm(10), decryptedText);
+            Console.WriteLine("This text is encrypted using ROT"+RotAlgorithm.CheckROTEncryption(decryptedText,encryptedText));
+            //RotAlgorithm.CheckROTEncryption(decryptedText,encryptedText); -> returns the ROT algorithm used to encrypt the decryptedText.
+        }
+    }
+}
---
+++ RotApplication/RotAlgorithm.cs
@@ -0,0 +1,102 @@
+﻿namespace RotApplication
+{
+    class RotAlgorithm
+    {
+        byte rotNumber; // number of rotations
+        public RotAlgorithm(byte rotNumber=13) // if there isn't any number defined as a parameter, 13 is the default value
+        {
+            this.rotNumber = rotNumber;
+        }
+
+        public static string Encrypt(RotAlgorithm rotAlg,string textToEncrypt) // takes as parameter an object of this class(in order to use the specified number of rotations) and the string to be encrypted.
+        {
+            // ASCII : a = 97 ; z = 122 ; A = 65 ; Z = 90;
+            string encryptedText ="";
+            foreach(char character in textToEncrypt)
+            {
+                if ((byte)character >= 97 && (byte)character <= 122) // checks if the character is between a-z
+                {
+                    if ((byte)character + rotAlg.rotNumber <= 122)
+                    {
+                        encryptedText += (char)((byte)character + rotAlg.rotNumber);
+                    }
+                    else
+                    {
+                        encryptedText += (char)(97 + (((byte)character - 1 + rotAlg.rotNumber) - 122)); // if number of rotations exceeds the ASCII value of z, the rest rotations will continue starting with the ASCII value of a
+                    }
+                }
+                else if ((byte)character >= 65 && (byte)character <= 90) // checks if the character is between A-Z
+                {
+                    if ((byte)character + rotAlg.rotNumber <= 90)
+                        encryptedText += (char)((byte)character + rotAlg.rotNumber);
+                    else
+                    {
+                        encryptedText += (char)(65 + (((byte)character - 1 + rotAlg.rotNumber) - 90));
+                    }
+                }
+                else
+                    encryptedText += character; // if character is not a letter, add it as it is
+            }
+            return encryptedText; // return the encrypted text.
+        }
+
+        public static string Decrypt(RotAlgorithm rotAlg, string textToDecrypt) // decrypts a text encrypted using a rot algorithm
+        {
+            string decryptedText = "";
+            foreach (char character in textToDecrypt)
+            {
+                if ((byte)character >= 97 && (byte)character <= 122) // checks if the character is between a-z
+                {
+                    if ((byte)character - rotAlg.rotNumber >= 97)
+                    {
+                        decryptedText += (char)((byte)character - rotAlg.rotNumber);
+                    }
+                    else // while decrypting, we have to decrease the ASCII value of the character with the number of rotations specified by the algorithm
+                    // so, in order to do that, we are subtracting from the ASCII value of z the number of rotations left after reaching the ASCII value
+                    // of a.
+                    {
+                        decryptedText += (char)(122 - ((97-((byte)character+1-rotAlg.rotNumber ))));
+                    }
+                }
+                else if ((byte)character >= 65 && (byte)character <= 90) // checks if the character is between A-Z
+                {
+                    if ((byte)character - rotAlg.rotNumber >= 65)
+                        decryptedText += (char)((byte)character - rotAlg.rotNumber);
+                    else
+                    {
+                        decryptedText += (char)(90 - ((65 - ((byte)character + 1 - rotAlg.rotNumber))));
+                    }
+                }
+                else
+                    decryptedText += character; // if character is not a letter, add it as it is
+            }
+            return decryptedText;
+        }
+
+        public static byte CheckROTEncryption(string decryptedText, string encryptedText)
+        {
+            byte rotAlgorithmUsed = 0; // this variable is going to be incremented for each rotation made untill the ASCII value of encryptedText == ASCII value of decryptedText
+            int indexOfChar = 0;
+            for(int i = 0; i < encryptedText.Length;i++)
+            {
+                if ((encryptedText[i] >= 65 && encryptedText[i] <= 90) || (encryptedText[i] >= 97 && encryptedText[i] <= 122)) // we only need the first character that is a letter from the string
+                {
+                    indexOfChar = i;
+                    break;
+                }
+            }
+            byte ASCIIofEncryptedChar = (byte)encryptedText[indexOfChar]; //converting the encrypted character to byte.
+            byte ASCIIofDecryptedChar = (byte)decryptedText[indexOfChar]; //converting the decrypted character to byte.
+            while(ASCIIofEncryptedChar != ASCIIofDecryptedChar)
+            {
+                rotAlgorithmUsed++; // incrementing the number of rotations made
+                ASCIIofEncryptedChar--;
+                if (ASCIIofEncryptedChar < 65) // if the encrypted character reaches the lower limit(ASCII value of 'a') we have to go to the upper limit(ASCII value of 'z')
+                    ASCIIofEncryptedChar = 90;
+                else if(ASCIIofEncryptedChar < 97 && ASCIIofEncryptedChar > 90) // same thing but for uppercase letters.
+                    ASCIIofEncryptedChar = 122;    
+            }
+            return rotAlgorithmUsed; //returning the number of rotations made as an int.
+        }
+    }
+}
